53bdaf0fc94ed77339d42356d88ad7ac75353346
t0x1c3500 <elmehdi.elboukili@gmail.com>
2024-11-09 21:42:59 +0100

    formatting line

---
+++ list_formatter.py
@@ -0,0 +1,8 @@
+formatted_lines = []
+with open('./list_2/inputs/list_2.txt', 'r') as file:
+    for line in file:
+        formatted_lines.append(f"https://{line.strip()}/collections/all?sort_by=created-descending")
+
+with open('./list_2/inputs/list_2-pre.txt', 'w') as file:
+    for line in formatted_lines:
+        file.write(line + '\n')
