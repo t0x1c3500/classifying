ce117fc5f2efd8884f50ceb951dbb4fc59454301
t0x1c3500 <elmehdi.elboukili@gmail.com>
2024-11-10 00:20:07 +0100

    update to current path

--- extract.py
+++ extract.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 
 links = []
 
-with open('./list_2/inputs/list_2-pre.txt', 'r') as file:
+with open('./list_3/inputs/list_3-pre.txt', 'r') as file:
     for line in file:
         line = line.strip()
         if line.startswith("http"):
@@ -100,7 +100,7 @@ def process_it(link, index):
     if len(final_products):
         domain = format_domain(link)
         print(f"Got Final Products For {domain}")
-        with open(f"./list_2/products/{domain}.json", "w") as link_write:
+        with open(f"./list_3/products/{domain}.json", "w") as link_write:
             link_write.write(json.dumps(final_products))
 
     return True
--- list_formatter.py
+++ list_formatter.py
@@ -1,8 +1,8 @@
 formatted_lines = []
-with open('./list_2/inputs/list_2.txt', 'r') as file:
+with open('./list_3/inputs/list_3.txt', 'r') as file:
     for line in file:
         formatted_lines.append(f"https://{line.strip()}/collections/all?sort_by=created-descending")
 
-with open('./list_2/inputs/list_2-pre.txt', 'w') as file:
+with open('./list_3/inputs/list_3-pre.txt', 'w') as file:
     for line in formatted_lines:
         file.write(line + '\n')
