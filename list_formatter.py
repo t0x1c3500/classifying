75a519356908fb7bfbc31835c85feb59797540cd
t0x1c3500 <elmehdi.elboukili@gmail.com>
2024-11-13 20:27:39 +0100

    update current path

--- list_formatter.py
+++ list_formatter.py
@@ -1,8 +1,8 @@
 formatted_lines = []
-with open('./list_11/inputs/list_11.txt', 'r') as file:
+with open('./list_12/inputs/list_12.txt', 'r') as file:
     for line in file:
         formatted_lines.append(f"https://{line.strip()}/collections/all?sort_by=created-descending")
 
-with open('./list_11/inputs/list_11-pre.txt', 'w') as file:
+with open('./list_12/inputs/list_12-pre.txt', 'w') as file:
     for line in formatted_lines:
         file.write(line + '\n')
