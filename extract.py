12ce71230fad6c2cb3e38b4425b6a5d6daa33ad4
t0x1c3500 <elmehdi.elboukili@gmail.com>
2024-11-25 19:26:05 +0100

    update current path

--- extract.py
+++ extract.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 
 links = []
 
-with open('./list_12-1/inputs/list_12-1-pre.txt', 'r') as file:
+with open('./list_13-1/inputs/list_13-1-pre.txt', 'r') as file:
     for line in file:
         line = line.strip()
         if line.startswith("http"):
@@ -100,7 +100,7 @@ def process_it(link, index):
     if len(final_products):
         domain = format_domain(link)
         print(f"Got Final Products For {domain}")
-        with open(f"./list_12-1/products/{domain}.json", "w") as link_write:
+        with open(f"./list_13-1/products/{domain}.json", "w") as link_write:
             link_write.write(json.dumps(final_products))
 
     return True
